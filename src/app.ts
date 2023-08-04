dd26812ca4ac3398233e61a82a9b40fe2f2861ad
Andrey Savelev <sandrig.dev@gmail.com>
2023-08-04 22:26:56 +0300

    feat: change command executor

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import { DirExecuter } from './commands/dir/dir.executor';
+import { FfmpegExecutor } from './commands/ffmpeg/ffmpeg.executor';
 import { ConsoleLogger } from './out/console-logger/console-logger';
 
 export class App {
   async run() {
-    new DirExecuter(ConsoleLogger.getInstance()).execute();
+    new FfmpegExecutor(ConsoleLogger.getInstance()).execute();
   }
 }
 
